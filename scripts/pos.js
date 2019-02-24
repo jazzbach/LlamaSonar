53335411a29bb4133183c7f886f7336a12d85e44
Can Bayraktarkatal <canbayraktarkatal@gmail.com>
2019-02-23 21:02:54 -0800

    Player Functions
    * Added pseudocodes for player functions.
    * Variables and functions will be updated.

---
+++ scripts/pos.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// This class is for defining 2D positions on the map by holding x and y coordinates.
+export default class Pos{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ scripts/routesystempair.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import Routes from './routes.js'
+import SystemPair from './systempairs.js'
+
+// This class is for holding route-system pairs to be used in engineer screen for self repair.
+// System pairs: [MINE + TORPEDO]: MT, [DRONE + SONAR]: DS, [SILENCE + SCENARIO]: SS
+// Areas: WEST: W, NORTH: N, SOUTH: S, EAST: E
+// Self repair route pairs: (Area, Component)
+//      - (W, DS), (W, SS), (W, MT), (E, MT)
+//      - (N, SS), (N, MT), (N, SS), (E, DS)
+//      - (S, DS), (S, SS), (S, MT), (E, SS)
+
